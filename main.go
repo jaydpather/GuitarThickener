c88c344ae18013daeac282066b494951d90f33ca
Jayd <jaydpather@gmail.com>
2020-10-17 09:41:29 -0500

    reading samples - extracted into function

--- main.go
+++ main.go
@@ -10,15 +10,21 @@ import (
 
 /*
 TODO:
-  * STEP 1: just copy file (don't modify)
-  * STEP 2: modify sample values
+  * MOST DEVS: main func loads file, modifies values and writes to file
+	* write the whole thing in 1 go, minimal testing.
+	  * just verify that it runs to the end, don't check for correct results
 
-  * extract simpleRead into a function that converts wav file into byte[][]
-	* each index in the byte[][] will be a byte[] of length 2, representing the 16-bit integer of the sample
-  * convert to int32
-	* iterate over byte[][]	
-		* convert each int 16 to int 32
-		* return an int32[]
+  * STEP 1: just copy file (don't modify)
+    * extract simpleRead into a new function
+    * get int32s - call ReadSample instead of ReadRawSample
+
+    * extract simpleSweep into a function
+	  * accepts int32[]
+	  * writes file to disk
+	    * takes # samples per second from source file
+	    * bits per sample is 32, b/c wav library only includes 32-bit output
+
+* STEP 2: modify sample values
   * process samples
 	* randomly modify each sample by +/- 1%
 	  * convert to float32
@@ -27,22 +33,17 @@ TODO:
 	  * apply delta value
 	  * cast modified float value to int32
 
-  * extract simpleSweep into a function
-	* accepts int32[]
-	* writes file to disk
-	  * takes # samples per second from source file
-	  * bits per sample is 32, b/c wav library only includes 32-bit output
-
 
 */
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("%i", len(os.Args))
-		fmt.Println("%A", os.Args)
-		fmt.Fprintf(os.Stderr, "Usage: simpleRead <file.wav>\n")
-		os.Exit(1)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
 	}
+}
+
+func readWavFile() []int {
 	testInfo, err := os.Stat(os.Args[1])
 	checkErr(err)
 
@@ -55,21 +56,31 @@ func main() {
 	fmt.Println("Hello, wav")
 	fmt.Println(wavReader)
 
-sampleLoop:
+	samples := []int{}
 	for {
-		s, err := wavReader.ReadRawSample()
+		curSample, err := wavReader.ReadSample()
 		if err == io.EOF {
-			break sampleLoop
+			break
 		} else if err != nil {
 			panic(err)
 		}
 
-		fmt.Printf("Sample: <%v>\n", s)
+		curSampleAsInt := int(curSample)
+
+		samples = append(samples, curSampleAsInt)
 	}
+
+	return samples
 }
 
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("%i", len(os.Args))
+		fmt.Println("%A", os.Args)
+		fmt.Fprintf(os.Stderr, "Usage: simpleRead <file.wav>\n")
+		os.Exit(1)
 	}
+
+	samples := readWavFile()
+	fmt.Println("%A", samples)
 }
